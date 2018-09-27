9786283c1a04f2bcb0eb04ddb8501b222c5c05a7
Juan Vitor <juanriven10@gmail.com>
2018-09-27 15:52:35 -0300

    commit dgo 27/09

--- js/clicker.js
+++ js/clicker.js
@@ -1,23 +1,55 @@
+let clicker, upgrades;
+
+class Upgrades {
+  constructor(){
+    this.upgrades = [
+      {
+        nome: "Hello, World!",
+        preco: "10b",
+        descricao: "primeiro upgrade",
+        modificadores: {batatasPSec: 0.1},
+        liberar: ["Agora ta chato"]
+      },
+      {
+        nome: "Agora ta chato",
+        preco: "100b",
+        descricao: "segundo upgrade",
+        modificadores: {batatasPSec: 0.1},
+        liberar: []
+      }
+    ];
+  }
+
+  getUpgrade(nome){
+    for(let upgrade of this.upgrades)
+      if(nome==upgrade.nome)
+        return upgrade;
+  }
+
+  convertPreco(preco){
+    
+  }
+
+  comprar(clicker,nome){
+    let upgrade = this.getUpgrade(nome);
+    if(clicker.status.batatas>=upgrade)
+  }
+}
+
 class BatataClicker {
   constructor() {
-    this.status = {
-      batatas: 0
-    };
+    this.status = {};
+    this.status.batatas= 0;
 
-    this.dom = {
-
-    }
+    this.dom = {};
+    this.dom.batataClick = document.querySelector("#batata-click");
+    this.dom.batataClick.addEventListener("click",function(){this.mostrarPontos()}.bind(this));
   }
 
   mostrarPontos(){
     this.status.batatas++;
     console.log(this.status.batatas);
   }
-
-  main(){
-    this.dom.batataClick = document.querySelector("#batata-click");
-    this.dom.batataClick.addEventListener("click",this.mostrarPontos);
-  }
 }
 
-(new BatataClicker()).main();
+(new BatataClicker());
