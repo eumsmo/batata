e493d784d3de81b076de98163fbe26d3e439d020
Juan Vitor <juanriven10@gmail.com>
2018-12-02 15:06:12 -0200

    A Terra e o Scroll alinhado
    - Scroll da fazenda inicia alinhado no centro
    - "Farm" foi dividida entre "Farm" e "Terra"

--- assets/js/sis.js
+++ assets/js/sis.js
@@ -1,7 +1,16 @@
 let fazendaEl = document.querySelector("#fazenda");
 
 fazendaEl.addEventListener("wheel",e=>{
-  console.log(e);
+  //console.log(e);
   e.preventDefault();
   return false;
 });
+
+fazendaEl.scrollIntoView({block: "center", inline: "center"});
+
+const centerVertScroll = ()=> fazenda.clientHeight - fazendaEl.firstElementChild.scrollHeight/2;
+const centerHoriScroll = ()=> fazenda.clientWidth - fazendaEl.firstElementChild.scrollWidth/2;
+const fazendaCenter = ()=> fazendaEl.scrollTo(centerHoriScroll(),centerVertScroll());
+
+window.addEventListener("resize",fazendaCenter);
+fazendaCenter();
